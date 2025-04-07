267898d2fe9d7809d235d4cfbd9e259c4474b6e7
anicca <anicca3584@gmail.com>
2025-04-07 14:16:58 +0700

    front-end tic-tac-toe

---
+++ resources/views/rex-runner.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Game Khủng Long</title>
+    <link rel="stylesheet" href="{{ asset('css/rex-runner.css') }}">
+  </head>
+  <body>
+    <div class="game">
+      <div id="dino"></div>
+      <div id="cactus"></div>
+    </div>
+    <script src="{{ asset('js/rex-runner.js') }}"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tic-tac-toe');
+});
+
+
+Route::get('/rex-runner', function () {
+    return view('rex-runner');
 });
